b9e7bd6368695c8caa5299854117ea10e0c29dc5
yaroslav.piskachov <yaroslav.piskachov@gmail.com>
2019-08-28 20:05:07 +0300

    Some configuration changes to run application properly

--- src/main/java/com/yaroslav/reactiveposts/ReactivePostsApplication.java
+++ src/main/java/com/yaroslav/reactiveposts/ReactivePostsApplication.java
@@ -2,8 +2,15 @@ package com.yaroslav.reactiveposts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@Import(UserRouter.class)
 
 @SpringBootApplication
+@EnableWebFlux
 public class ReactivePostsApplication {
 
     public static void main(String[] args) {
